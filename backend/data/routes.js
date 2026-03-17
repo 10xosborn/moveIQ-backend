fd59c3b15e0de107abf0896f601e5cef209c179e
Toluwanimi Akinleye <akinleyetoluwanimi@gmail.com>
2026-03-16 17:14:33 -0700

    refactor: align directory structure with ESM and seed Lagos routes

---
+++ backend/data/routes.js
@@ -0,0 +1,84 @@
+const routesData = [
+  { 
+    name: "Ikorodu Road", 
+    area: "Mainland", 
+    junctions: ["Maryland", "Ojota", "Ketu", "Fadeyi"],
+    startLocation: "Ikorodu",
+    endLocation: "Maryland",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1" // Placeholder ID
+  },
+  { 
+    name: "Third Mainland Bridge", 
+    area: "Island", 
+    junctions: ["Oworo", "Adekunle", "Adeniji Adele"],
+    startLocation: "Oworo",
+    endLocation: "Adeniji",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Ozumba Mbadiwe", 
+    area: "Island", 
+    junctions: ["Law School", "1004", "Oriental Hotel"],
+    startLocation: "Victoria Island",
+    endLocation: "Lekki Phase 1",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Lekki-Epe Expressway", 
+    area: "Island", 
+    junctions: ["Ajah", "Abraham Adesanya", "VGC", "Eleko"],
+    startLocation: "Lekki Toll",
+    endLocation: "Epe",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Agege Motor Road", 
+    area: "Mainland", 
+    junctions: ["Mushin", "Oshodi", "Ikeja Along", "Capitall"],
+    startLocation: "Agege",
+    endLocation: "Oshodi",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Funsho Williams Avenue", 
+    area: "Mainland", 
+    junctions: ["Barracks", "Ojuelegba", "Stadium", "Iponri"],
+    startLocation: "Surulere",
+    endLocation: "Costain",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Ikotun–Egbeda Road", 
+    area: "Mainland", 
+    junctions: ["Iyana Ipaja", "Egbeda Bus Stop", "Ikotun Last Bus Stop"],
+    startLocation: "Iyana Ipaja",
+    endLocation: "Ikotun",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Falomo Bridge", 
+    area: "Island", 
+    junctions: ["Ikoyi Side", "Victoria Island Side"],
+    startLocation: "Ikoyi",
+    endLocation: "Victoria Island",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Apapa-Oshodi Expressway", 
+    area: "Industrial", 
+    junctions: ["Cele", "Iyana Itire", "Mile 2", "Berliet"],
+    startLocation: "Apapa",
+    endLocation: "Oshodi",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  },
+  { 
+    name: "Lagos-Ibadan Expressway", 
+    area: "Industrial", 
+    junctions: ["Berger", "7UP", "Kara", "Long Bridge"],
+    startLocation: "Berger",
+    endLocation: "Sagamu Interchange",
+    createdBy: "65f1a2b3c4d5e6f7a8b9c0d1"
+  }
+];
+
+export default routesData;
