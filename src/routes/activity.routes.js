c35a6e934607c94595275e3c19aaccbcda4b415a
Elizabeth Edward <young2kauru@gmail.com>
2026-03-18 10:10:55 +0100

    Add activity feed endpoint and connect routes

--- src/routes/activity.routes.js
+++ src/routes/activity.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getActivitiesFeed } from "../controllers/activity.controller.js";
+import protect from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", getActivitiesFeed);
+router.get("/", protect, getActivitiesFeed);
 
 export default router;
